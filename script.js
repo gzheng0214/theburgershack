f7bcdd394d2a1047f78f03fee2b3ddc2a3ed44b4
gavin33361 <gzheng0214@gmail.com>
2021-01-13 20:49:03 -0500

    Finished desktop navigation bar

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Gavin
+* @Date:   2021-01-13 20:36:28
+* @Last Modified by:   Gavin
+* @Last Modified time: 2021-01-13 20:38:31
+*/
+window.addEventListener('load', function(){
+	document.querySelector('.header__image-text').style.opacity = 1;
+
+	document.querySelector('.header__image-text').style.visibility = 'visible';
+});
